e0aabe8e70e8a557d73a487ccc191dddfa38a69d
komangsu <mangokky@gmail.com>
2021-11-02 13:54:08 +0800

    create app hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/komangsu/app-hello
+
+go 1.17
+
+require github.com/komangsu/go-module-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	h "github.com/komangsu/go-module-hello"
+)
+
+func main() {
+	fmt.Println(h.Hello())
+}
